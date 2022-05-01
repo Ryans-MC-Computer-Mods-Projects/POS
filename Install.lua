7bc0e70dc80b39318e83257390bfa3d959892279
Ryan <99054441+Ryans-MC-Computer-Mods-Projects@users.noreply.github.com>
2022-05-01 19:30:51 -0400

    Update Install.lua

--- Install.lua
+++ Install.lua
@@ -1,26 +1,26 @@
-local RTKrnl = http.get("https://raw.githubusercontent.com/Ryans-MC-Computer-Mods-ProjectsPOS/main/Installer/RTKrnl.lua")
+local RTKrnl = http.get("https://raw.githubusercontent.com/Ryans-MC-Computer-Mods-Projects/POS/main/Installer/RTKrnl.lua")
 local fh = fs.open("/temp/RTKrnl.lua", "w")
-fh.write(fh.readAll())
+fh.write(RTKrnl.readAll())
 fh.close()
 
 local Desktop = http.get("https://raw.githubusercontent.com/Ryans-MC-Computer-Mods-Projects/POS/main/Installer/Desktop.lua")
 local fh = fs.open("/temp/Desktop.lua", "w")
-fh.write(fh.readAll())
+fh.write(Desktop.readAll())
 fh.close()
 
 local Taskbar = http.get("https://raw.githubusercontent.com/Ryans-MC-Computer-Mods-Projects/POS/main/Installer/Taskbar.lua")
 local fh = fs.open("/temp/Taskbar.lua", "w")
-fh.write(fh.readAll())
+fh.write(Taskbar.readAll())
 fh.close()
 
 local System = http.get("https://raw.githubusercontent.com/Ryans-MC-Computer-Mods-Projects/POS/main/Installer/System.lua")
 local fh = fs.open("/temp/System.lua", "w")
-fh.write(fh.readAll())
+fh.write(System.readAll())
 fh.close()
 
 local installer = http.get("https://raw.githubusercontent.com/Ryans-MC-Computer-Mods-Projects/POS/main/Installer/installer.lua")
 local fh = fs.open("/temp/installer.lua", "w")
-fh.write(fh.readAll())
+fh.write(installer.readAll())
 fh.close()
 
 shell.run("/temp/RTKrnl")
